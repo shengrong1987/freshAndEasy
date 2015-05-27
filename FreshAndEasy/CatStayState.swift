4de5326a44f66d13035d01cb873cb389d23687fe
ShengRong <aimbebe.r@gmail.com>
2015-05-27 16:43:37 -0700

    some little changes

---
+++ FreshAndEasy/CatStayState.swift
@@ -0,0 +1,23 @@
+//
+//  CatStayState.swift
+//  FreshAndEasy
+//
+//  Created by sheng rong on 15/5/27.
+//  Copyright (c) 2015年 Aimbebe. All rights reserved.
+//
+
+import Foundation
+
+class CatStayState : State{
+    func exitState(cat: Cat) {
+        
+    }
+    
+    func excuteState(cat: Cat) {
+        
+    }
+    
+    func enterState(cat: Cat) {
+        
+    }
+}
---
+++ FreshAndEasy/State.swift
@@ -0,0 +1,15 @@
+//
+//  State.swift
+//  FreshAndEasy
+//
+//  Created by sheng rong on 15/5/27.
+//  Copyright (c) 2015年 Aimbebe. All rights reserved.
+//
+
+import Foundation
+
+protocol State{
+    func exitState(cat:Cat)
+    func excuteState(cat:Cat)
+    func enterState(cat:Cat)
+}
